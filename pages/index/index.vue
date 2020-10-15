67822cbea11030d5362eb3372b5f7e83d1e6b6dc
zkj <1990562930@qq.com>
2020-10-15 22:12:04 +0800

    继续提交了首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -6,7 +6,7 @@
 				<view class="searchBox">
 					<image class="icon" src="http://yanxuan.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/search2-2fb94833aa.png"
 					 mode=""></image>
-					 <!-- 动态数量 -->
+					<!-- 动态数量 -->
 					<text class="text">商品搜索，共300款好物</text>
 				</view>
 			</navigator>
@@ -63,7 +63,7 @@
 
 		<view class="newpro">
 			<view class="sunNewPro" v-for="item in newGoods" :key='item.id'>
-				<view  @click="sunnewGood(item.id)">
+				<view @click="sunnewGood(item.id)">
 					<image :src="item.list_pic_url" mode=""></image>
 					<text class="name">{{item.name}}</text>
 					<text class="price">${{item.retail_price}}</text>
@@ -76,18 +76,28 @@
 				<text class="supplyText">人气推荐</text>
 			</navigator>
 		</view>
-		
+
 		<!-- 人气推荐商品信息 -->
 		<view class="top">
-			<navigator url="" class="topGood">
-				<image src="http://yanxuan.nosdn.127.net/583812520c68ca7995b6fac4c67ae2c7.png" mode=""></image>
+			<view class="topGood" v-for="item in hotGoodsList" :key='item.id' @click="sunnewGood(item.id)">
+				<image :src="item.list_pic_url" mode=""></image>
+				<view class="topText">
+					<text class="title">{{item.name}}</text><br>
+					<text class="detail">{{item.goods_brief}}</text><br>
+					<text class="top-price">￥{{item.retail_price}}</text>
+				</view>
+			</view>
+		</view>
+
+		<!-- 人气推荐 -->
+		<view class="supply">
+			<navigator url="../topic/topic">
+				<text class="supplyText">专题精选</text>
 			</navigator>
 		</view>
 
-
-
-
-
+		<!-- swiper -->
+		<special-banner :banner-list="bannerList" :swiper-config="swiperConfig"></special-banner>
 
 	</view>
 </template>
@@ -96,6 +106,7 @@
 	import {
 		getHomeLunbo
 	} from '@/api/homeApi.js';
+	import specialBanner from '../../components/EtherealWheat-banner/specialBanner.vue'
 	export default {
 		data() {
 			return {
@@ -106,11 +117,45 @@
 				// 品牌制造商list
 				BrandList: [],
 				// 新品首发
-				newGoods : [],
+				newGoods: [],
+				// 人气推荐
+				hotGoodsList: [],
 				indicatorDots: true,
 				autoplay: true,
 				interval: 2000,
-				duration: 500
+				duration: 500,
+				bannerList: [{
+					picture: 'http://image.mishi.cn/r/yry_h5_test/detail/3_1535359279285.png',
+					title: '七夕将至：时光足够久，韧性也能炖出味',
+					description: '一万年太久，就现在，给你爱',
+					path: ''
+				}, {
+					picture: 'http://image.mishi.cn/r/yry_h5_test/detail/2_1535359240426.png',
+					title: '新菜上架：无边海洋，找到顺眼的那尾鱼',
+					description: '花中樱，鱼乃鲷花中樱，鱼乃鲷',
+					path: ''
+				}, {
+					picture: 'http://image.mishi.cn/r/yry_h5_test/detail/1_1535359204228.png',
+					title: '在湘西的烟火气里，发现苗族少女的神明',
+					description: '取材自湘西苗族传统的烟熏文化',
+					path: ''
+				}, {
+					picture: 'http://image.mishi.cn/r/yry_h5_test/detail/4_1535359327213.png',
+					title: '福利降临，陪伴独自行走的丰盛旅程',
+					description: '在自己的小世界里，日日好日，夜夜好清宵',
+					path: ''
+				}],
+				swiperConfig: {
+					indicatorDots: true,
+					indicatorColor: 'rgba(255, 255, 255, .4)',
+					indicatorActiveColor: 'rgba(255, 255, 255, 1)',
+					autoplay: false,
+					interval: 3000,
+					duration: 300,
+					circular: true,
+					previousMargin: '58rpx',
+					nextMargin: '58rpx'
+				}
 			}
 		},
 		methods: {
@@ -127,20 +172,28 @@
 				this.BrandList = data.brandList;
 				// 新品首发
 				this.newGoods = data.newGoodsList;
+				// 人气推荐
+				this.hotGoodsList = data.hotGoodsList;
+				// 专题精选
+				this.topicList = data.topicList
 			},
 			//商品详情
-			sunnewGood(id){
+			sunnewGood(id) {
 				console.log(id)
 				uni.navigateTo({
-					url:"../goods/goods?id="+id
-					
+					url: "../goods/goods?id=" + id
+
 				})
-				
+
 			},
 		},
 		created() {
 			this.getLunboDate();
+		},
+		components:{
+			"special-banner" :specialBanner 
 		}
+		
 	}
 </script>
 
@@ -263,8 +316,10 @@
 			height: 504rpx;
 			display: flex;
 			flex-wrap: wrap;
+
 			.manubox {
 				position: relative;
+
 				.manuImg {
 					width: 375rpx;
 					height: 253rpx;
@@ -274,22 +329,26 @@
 						height: 100%;
 					}
 				}
+
 				.manuText {
 					position: absolute;
 					z-index: 2;
 					padding: 27rpx 31rpx;
 					left: 0;
 					top: 0;
+
 					.btand {
 						display: block;
 						font-size: 33rpx;
 						height: 43rpx;
 						color: #333;
 					}
+
 					.price {
 						font-size: 25rpx;
 						color: #999;
 					}
+
 					.unit {
 						font-size: 25rpx;
 						color: #999;
@@ -331,21 +390,68 @@
 					line-height: 30rpx;
 					font-size: 30rpx;
 					color: red;
-				
-					 overflow: hidden;
-					  text-overflow: ellipsis;
-					  white-space: nowrap;
+
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
 				}
 			}
 		}
+
 		.top {
-			height: 264rpx;
-			width:100%;
-			background-color: pink;
+			height: auto;
+			width: 100%;
+			background-color: #FFFFFF;
+			font-size: 29rpx;
+			color: #333333;
+
 			.topGood {
+				// background-color: blue;
+				border-top: 1px solid #d9d9d9;
+				margin: 0 20rpx;
+				height: 264rpx;
+				width: 710rpx;
+				display: flex;
+
 				image {
-					    width: 240rpx;
-					    height: 240rpx;
+
+					width: 240rpx;
+					height: 240rpx;
+				}
+
+				.topText {
+					display: flex;
+					flex-direction: column;
+					flex-wrap: nowrap;
+					justify-content: center;
+					overflow: hidden;
+					height: 264rpx;
+					width: 456rpx;
+
+					.title {
+						width: 456rpx;
+						display: block;
+						line-height: 50rpx;
+						font-size: 30rpx;
+						color: #333;
+					}
+
+					.detail {
+						width: 456rpx;
+						display: block;
+						line-height: 50rpx;
+						font-size: 25rpx;
+						color: #999;
+
+					}
+
+					.top-price {
+						width: 456rpx;
+						display: block;
+						line-height: 50rpx;
+						font-size: 33rpx;
+						color: #b4282d;
+					}
 				}
 			}
 		}
