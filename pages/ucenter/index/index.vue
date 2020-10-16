6ee1f103c88a33abed5333b34992c562b59e8e79
homi <2392462019@qq.com>
2020-10-16 10:44:03 +0800

    修改page.json文件

--- pages/ucenter/index/index.vue
+++ pages/ucenter/index/index.vue
@@ -16,7 +16,20 @@
 				<text class="icon" :class="item.class" ></text>
 				<text class="txt">{{item.txt}}</text>
 			</view> -->
-			
+			<u-grid :col="3">
+				<u-grid-item>
+					<u-icon name="photo" :size="46"></u-icon>
+					<view class="grid-text">图片</view>
+				</u-grid-item>
+				<u-grid-item>
+					<u-icon name="lock" :size="46"></u-icon>
+					<view class="grid-text">锁头</view>
+				</u-grid-item>
+				<u-grid-item>
+					<u-icon name="hourglass" :size="46"></u-icon>
+					<view class="grid-text">沙漏</view>
+				</u-grid-item>
+			</u-grid>
 		</view>
 		<view class="dialog-login" @click="onCloseLoginDialog" v-if="showLoginDialog" >
 			<view class="dialog-body">
