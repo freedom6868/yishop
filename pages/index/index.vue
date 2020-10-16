0549535b4e695d4c91997ca4fafa60e31d38b2c2
Your Name <you@example.com>
2020-10-16 08:58:26 +0800

    修改index.vue冲突

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,4 @@
 <template>
-<<<<<<< HEAD
-	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
-=======
 	<view class="indexBox">
 		<!-- 首页搜索框 -->
 		<view class="search">
@@ -105,30 +99,10 @@
 		<!-- swiper -->
 		<special-banner :banner-list="bannerList" :swiper-config="swiperConfig"></special-banner>
 
->>>>>>> dev
 	</view>
 </template>
 
 <script>
-<<<<<<< HEAD
-	export default {
-		data() {
-			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
-			}
-		},
-		methods: {
-
-		}
-	}
-</script>
-
-<style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
-=======
 	import {
 		getHomeLunbo
 	} from '@/api/homeApi.js';
@@ -481,6 +455,5 @@
 				}
 			}
 		}
->>>>>>> dev
 	}
 </style>
