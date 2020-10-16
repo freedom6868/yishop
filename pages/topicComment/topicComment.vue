4b7121878740483b651f94a19b1ffd4aacbf33c2
Your Name <you@example.com>
2020-10-16 16:55:50 +0800

    创建查看更多评论页面

---
+++ pages/topicComment/topicComment.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		查看更多评论页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
