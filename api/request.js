72be02bfa0e09f11724a86f1ebf3e7303416cb1e
homi <2392462019@qq.com>
2020-10-24 14:15:11 +0800

    修改接口地址

--- api/request.js
+++ api/request.js
@@ -1,4 +1,5 @@
-const baseURL = 'http://47.106.90.23:8360/api/';
+// const baseURL = 'http://47.106.90.23:8360/api/';
+const baseURL = http://shopapi.homi168.com/api/;
 
 function request(option){
 	return new Promise((resolve,reject)=>{
