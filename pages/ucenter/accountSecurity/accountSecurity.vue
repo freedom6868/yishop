f7f8b87184d41c7367614435adead34d9465fb26
zkj <1990562930@qq.com>
2020-10-24 16:03:58 +0800

    提交联系客服功能

--- pages/ucenter/accountSecurity/accountSecurity.vue
+++ pages/ucenter/accountSecurity/accountSecurity.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-			账号安全
+			<template>
+				<u-cell-group>
+					<u-cell-item icon="weixin-fill" title="微信名" :value="nickname"></u-cell-item>
+					<u-cell-item icon="phone-fill" title="手机号" value="eg:15913953648"></u-cell-item>
+				</u-cell-group>
+				
+				
+			</template>
 	</view>
 </template>
 
@@ -8,11 +15,20 @@
 	export default {
 		data() {
 			return {
-				
+				nickname:''
 			}
 		},
 		methods: {
-			
+			getUserData(){
+				var userData = JSON.parse( uni.getStorageSync('userInfo'));
+				console.log(userData)
+				this.nickname = userData.nickname;
+				console.log(this.nickname)
+			}
+		},
+		
+		onLoad(){
+			this.getUserData();
 		}
 	}
 </script>
--- pages/ucenter/relationService/relationService.vue
+++ pages/ucenter/relationService/relationService.vue
@@ -1,6 +1,14 @@
 <template>
 	<view>
-			客服
+		<view class="contact-icon">
+			<text style="color: #F04A44;" class="icon iconfont icon-kefu"></text>
+			<button class="kefu" open-type="contact">联系客服</button>
+		</view>
+		<button class='button' @click='addWechat'>复制添加客服微信</button>
+		<button class='button' @click="phone">点击拨打客服电话</button>
+
+
+
 	</view>
 </template>
 
@@ -8,11 +16,35 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+			//启动拨号功能
+			phone(){
+				uni.makePhoneCall({
+					phoneNumber: '15913953648', //客服电话
+					success: function() {
+						console.log('成功拨打电话')
+					}
+				})
+			},
+			// 添加客服微信
+			addWechat() {
+				uni.setClipboardData({
+					data: 'zkj5396',
+					success: function(res) {
+						uni.getClipboardData({
+							success: function(res) {
+								console.log('复制成功')
+								console.log(res.data) // data
+							}
+						})
+					}
+				})
+			},
+
+
 		}
 	}
 </script>
